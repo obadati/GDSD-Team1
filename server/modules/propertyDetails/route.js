4266256855ec6742c2b6e82662f5faf45be8fa9f
tahabinaziz <thzz9574@gmail.com>
2021-05-21 21:20:36 +0500

    Pagination on property

--- server/modules/propertyDetails/route.js
+++ server/modules/propertyDetails/route.js
@@ -5,14 +5,14 @@ const controller = require('./controller');
 /*Create Properties */
 router.post("/",controller.create);
 
-/*All Properties */
-router.get("/",controller.getAllProperty);
-
 /*Filter Property By Text */
-router.get("/search",controller.searchPropertyByText);
+router.get("/search/",controller.searchPropertyByText);
+
+/*All Properties */
+router.get("/:page",controller.getAllProperty);
 
 /*Filter Property By Category */
-router.get("/category/:id",controller.propertyByCategoryId);
+router.get("/category/:page",controller.propertyByCategoryId);
 
 /*Delete Property */
 router.delete("/:id",controller.deleteProperty)
