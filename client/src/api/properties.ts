7ce0ef28a13f7109f83fc8b2b636b9fbb73eb2b3
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-05-25 00:57:52 +0200

    Added search api endpoint

--- client/src/api/properties.ts
+++ client/src/api/properties.ts
@@ -2,7 +2,7 @@ import { httpGET } from "../utility/http";
 
 export enum PropertiesEndpoints {
   GetAll = "/api/properties/",
-  SearchProperties = "api/properties/search",
+  SearchProperties = "/api/properties/search",
 }
 export const BASE_URL = "http://18.185.96.197:5000";
 
@@ -12,6 +12,6 @@ export const getAllProperties = (page: number = 1) => {
 
 export const searchForProperties = (query: string, page = 1) => {
   return httpGET(
-    `${BASE_URL}${PropertiesEndpoints.SearchProperties}?text=${query}&page=${page}`
+    `${BASE_URL}${PropertiesEndpoints.SearchProperties}/?text=${query}&page=${page}`
   );
 };
