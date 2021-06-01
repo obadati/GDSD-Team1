86e23249cbc5f01e2aec7c5d233809438d40561e
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-06-01 15:34:50 +0200

    Improve carousel component

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -5,11 +5,23 @@ import "./Carousel.scss";
 
 interface CarouselProps {
   images?: any[];
+  rounded?: boolean;
 }
-const CarouselComponent: React.FC<CarouselProps> = ({ images = [] }) => {
+const CarouselComponent: React.FC<CarouselProps> = ({
+  images = [],
+  rounded = false,
+}) => {
   return (
-    <div className='carousel-component'>
-      <Carousel autoPlay>
+    <div
+      className={`carousel-component carousel-component--${
+        rounded ? "rounded" : ""
+      }`}>
+      <Carousel
+        animation='slide'
+        interval={3000}
+        cycleNavigation
+        autoPlay
+        indicators={false}>
         {images.map((item, i) => (
           <div className='slide' key={i}>
             <img src={`${BASE_URL}/${item}`}></img>
