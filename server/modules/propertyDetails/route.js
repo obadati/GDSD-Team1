d95a34e27515a724ee8fbd4730264400553a573b
tahabinaziz <thzz9574@gmail.com>
2021-06-07 11:10:37 +0500

    changes  propertyDetail v1.1

--- server/modules/propertyDetails/route.js
+++ server/modules/propertyDetails/route.js
@@ -1,9 +1,9 @@
+  
 const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
 /**********************************Register Controller & Define Routing Path*****************************/
-
 
 /***********************************************Agent Dashboard******************************************/
 
