1cc0b27a1dbf0b6b81254dd4ccaeda12a8906978
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-06-08 14:05:15 +0200

    Minor formatting and btn color fix

--- client/src/pages/AboutUs/component/Contact.tsx
+++ client/src/pages/AboutUs/component/Contact.tsx
@@ -14,67 +14,67 @@ const ContactUs: React.FC<any> = () => {
   });
 
   const { name, description, email, phone } = user;
-  const onInputChange = (e:any) => {
+  const onInputChange = (e: any) => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
-  const onSubmit = async (e:any) => {
+  const onSubmit = async (e: any) => {
     e.preventDefault();
-   await craeteContact(user)
+    await craeteContact(user);
     // await axios.post("http://18.185.96.197:5000/api/contactUs/", user);
     history.push("/");
   };
   return (
     <div>
-      <div className="container">
-        <div className="Content">
-            <div className="w-100    mx-auto shadow p-5">
-              <h2 className="text-center mb-4">Contact Us</h2>
-              <form onSubmit={(e) => onSubmit(e)}>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="Enter Your Name"
-                    name="name"
-                    value={name}
-                    onChange={(e) => onInputChange(e)}
-                  />
-                </div>
-                
-                <div className="form-group">
-                  <input
-                    type="email"
-                    className="form-control form-control-lg"
-                    placeholder="Enter Your E-mail Address"
-                    name="email"
-                    value={email}
-                    onChange={(e) => onInputChange(e)}
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="Enter Your Phone Number"
-                    name="phone"
-                    value={phone}
-                    onChange={(e) => onInputChange(e)}
-                  />
-                </div>
-                <div className="form-group">
-                  <textarea
-                    
-                    className="form-control form-control-lg"
-                    placeholder="Enter Your Query"
-                    name="description"
-                    value={description}
-                    onChange={(e) => onInputChange(e)}
-                  />
-                  
-                </div>
-                <button className="btn btn-danger btn-block" onClick={onSubmit}>Send Query</button>
-              </form>
-            </div>
+      <div className='container'>
+        <div className='Content'>
+          <div className='w-100    mx-auto shadow p-5'>
+            <h2 className='text-center mb-4'>Contact Us</h2>
+            <form onSubmit={(e) => onSubmit(e)}>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  className='form-control form-control-lg'
+                  placeholder='Enter Your Name'
+                  name='name'
+                  value={name}
+                  onChange={(e) => onInputChange(e)}
+                />
+              </div>
+
+              <div className='form-group'>
+                <input
+                  type='email'
+                  className='form-control form-control-lg'
+                  placeholder='Enter Your E-mail Address'
+                  name='email'
+                  value={email}
+                  onChange={(e) => onInputChange(e)}
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  className='form-control form-control-lg'
+                  placeholder='Enter Your Phone Number'
+                  name='phone'
+                  value={phone}
+                  onChange={(e) => onInputChange(e)}
+                />
+              </div>
+              <div className='form-group'>
+                <textarea
+                  className='form-control form-control-lg'
+                  placeholder='Enter Your Query'
+                  name='description'
+                  value={description}
+                  onChange={(e) => onInputChange(e)}
+                />
+              </div>
+              <button className='btn btn-success btn-block' onClick={onSubmit}>
+                Send Query
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
