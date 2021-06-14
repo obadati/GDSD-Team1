9b36b6e516e7f45a265bb2ccc6a2165cd182cf7b
Obada Altinawi <obada.altinawi@informatik.hs-fulda.de>
2021-06-14 17:02:22 +0200

    Add index

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,5 @@
 export { default as AboutPage } from "./About/About";
 export { default as LandingPage } from "./Landing/Landing";
 export { default as HomePage } from "./Home/Home";
+export { default as MessengerPage } from "./Messenger/Messenger";
 export { default as PropertiesPage } from "./Properties/Properties";
