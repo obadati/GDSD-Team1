2a48324d084496997b3d5d92dfbf80258d3794e4
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-06-15 17:41:16 +0200

    Use id

--- client/src/hooks/auth.ts
+++ client/src/hooks/auth.ts
@@ -4,16 +4,17 @@ export const useAuth = (): {
     authenticated: boolean;
     username: string;
     token: string;
+    id: number;
 } => {
     const user = localStorage.getItem(AUTH_USER_KEY) || null;
     if (!user) {
-        return { authenticated: false, username: "", token: "" };
+        return { authenticated: false, username: "", token: "", id: -1 };
     }
 
     const authUser = JSON.parse(user);
-    let { token = "", username = "", email = "" } = authUser;
+    let { token = "", username = "", email = "", id } = authUser;
     if (!username && email) {
         username = email.split("@")[0];
     }
-    return { token, username, authenticated: Boolean(token) };
+    return { token, username, authenticated: Boolean(token), id };
 };
