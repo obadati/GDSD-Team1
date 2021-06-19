a23f86e957faf64a797791749e7b2b65c41576d2
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-06-19 05:46:38 +0200

    Improve UX dashboard

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -5,38 +5,46 @@ import { useAuth } from "../../hooks/auth";
 import "./Dashboard.scss";
 import { NavigationTab } from "../../store/navigation/types";
 import { useState } from "react";
-import AgentProperties from "../AgentProperties/AgentProperties";
+import x from "../../assets/images/hero-fallback-2.jpg";
 
 const Dashboard: React.FC<any> = () => {
-  const { username } = useAuth();
-  const [selectedTab, setSelectedTab] = useState<number>(-1);
-  const dashboardTabs: NavigationTab[] = [
-    {
-      label: "Properties",
-      to: `${AppRoutes.Properties}`,
-    },
-  ];
-  return (
-    <div className='app-page user-dashboard '>
-      <div className='user-dashboard__aside'>
-        {dashboardTabs.map((tab, index) => (
-          <a
-            className={`${index === selectedTab} ? 'selected': ""`}
-            onClick={() => setSelectedTab(index)}>
-            {tab.label}
-          </a>
-        ))}
-      </div>
-      <div className='user-dashboard__center'>
-        {selectedTab === -1 && (
-          <p>
-            Welcome <span className='username'> {username}</span>
-          </p>
-        )}
-        {selectedTab === 0 && <AgentProperties></AgentProperties>}
-      </div>
-    </div>
-  );
+    const { username } = useAuth();
+    const [selectedTab, setSelectedTab] = useState<number>(-1);
+    const tabs: NavigationTab[] = [
+        {
+            label: "Properties",
+            to: `${AppRoutes.Properties}`,
+        },
+        {
+            label: "Messages",
+            to: `${AppRoutes.Messenger}`,
+        },
+        {
+            label: "Companies",
+            to: `${AppRoutes.Companies}`,
+        },
+        {
+            label: "Contracts",
+            to: `${AppRoutes.Contracts}`,
+        },
+        {
+            label: "Approvals",
+            to: `${AppRoutes.Approvals}`,
+        },
+    ];
+    return (
+        <div className="app-page user-dashboard ">
+            <div className="user-dashboard__tiles">
+                {tabs.map((tab) => (
+                    <Link to={tab.to}>
+                        <div className="user-dashboard__tiles__tile">
+                            <p>{tab.label}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
