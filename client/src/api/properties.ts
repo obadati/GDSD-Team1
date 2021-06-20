16c0ba89f148ae995d657c4e562a7c3a8ec7ac6a
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-06-20 19:19:04 +0200

    Wire up api

--- client/src/api/properties.ts
+++ client/src/api/properties.ts
@@ -6,7 +6,7 @@ export enum PropertiesEndpoints {
     SearchProperties = "/api/properties/search",
     SearchByCategory = "/api/properties/category",
     GetAgentProperties = "/api/properties/agentProperty/:page?agentId=:agentId",
-    UpdateProperty = "/api/properties/agentProperty/:uid?agentId=:agentUid ",
+    UpdateProperty = "/api/properties/:uid",
 }
 
 export const BASE_URL = "http://18.185.96.197:5000";
@@ -37,11 +37,18 @@ export const searchByCategory = (categoryId: number, page = 1) => {
 };
 
 export const updateProperty = (property: Property, agentUid: string) => {
+    const {
+        title,
+        description,
+        price,
+        location,
+        room,
+        size,
+        category: { id: categoryId },
+        images,
+    } = property;
     return httpPOST(
-        PropertiesEndpoints.UpdateProperty.replace(":uid", property.id).replace(
-            ":agentUid",
-            agentUid
-        ),
+        PropertiesEndpoints.UpdateProperty.replace(":uid", property.id),
         {}
     );
 };
