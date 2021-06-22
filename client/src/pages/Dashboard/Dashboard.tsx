1959e8016a102a180930c9028fc00c9503af086c
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-06-22 16:26:41 +0200

    Added assets

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -7,26 +7,39 @@ import { NavigationTab } from "../../store/navigation/types";
 import { useState } from "react";
 import x from "../../assets/images/hero-fallback-2.jpg";
 import { UserRoles } from "../../api/user";
+import contractIcon from "../../assets/images/contract.png";
+import homeIcon from "../../assets/images/home.png";
+import msgIcon from "../../assets/images/msg.png";
+import companyIcon from "../../assets/images/company.png";
+import approvalIcon from "../../assets/images/approval.png";
+
+interface DashboardTile extends NavigationTab {
+    icon: string;
+}
 
 const Dashboard: React.FC<any> = () => {
     const { username, role } = useAuth();
     const [selectedTab, setSelectedTab] = useState<number>(-1);
-    const tabs: NavigationTab[] = [
+    const tabs: DashboardTile[] = [
         {
             label: "Messages",
             to: `${AppRoutes.Messenger}`,
+            icon: msgIcon,
         },
         {
             label: "Companies",
             to: `${AppRoutes.Companies}`,
+            icon: companyIcon,
         },
         {
             label: "Contracts",
             to: `${AppRoutes.Contracts}`,
+            icon: contractIcon,
         },
         {
             label: "Approvals",
             to: `${AppRoutes.Approvals}`,
+            icon: approvalIcon,
         },
     ];
 
@@ -34,6 +47,7 @@ const Dashboard: React.FC<any> = () => {
         tabs.push({
             label: "Properties",
             to: `${AppRoutes.Properties}`,
+            icon: homeIcon,
         });
     }
 
@@ -43,7 +57,10 @@ const Dashboard: React.FC<any> = () => {
                 {tabs.map((tab) => (
                     <Link to={tab.to}>
                         <div className="user-dashboard__tiles__tile">
-                            <p>{tab.label}</p>
+                            <div className="tile-content-wrapper">
+                                <img src={tab.icon} alt="" />
+                                <p>{tab.label}</p>
+                            </div>
                         </div>
                     </Link>
                 ))}
