a052e5bb7d147e4f70ea8fba8c0ad8e880e547b1
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-06-27 22:02:15 +0200

    Fix minor style leak

--- client/src/pages/ApprovalManagement/ApprovalDashboard/Approval.tsx
+++ client/src/pages/ApprovalManagement/ApprovalDashboard/Approval.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import propertyImage from "../../../assets/images/property.png";
 import agentImage from "../../../assets/images/agent.png";
 import "./Approval.scss";
 const Approval = () => {
     return (
-        <div>
+        <div className="approvals-page">
             <div>
-                \
                 <div className="wrapper">
                     <div className="row">
                         <div className="col col style-box height">
