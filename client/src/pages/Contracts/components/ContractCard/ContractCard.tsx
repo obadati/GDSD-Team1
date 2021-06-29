c056c3166fe7185faa4063348ff6c9a1487bde86
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-06-29 17:42:27 +0200

    Sanity check

--- client/src/pages/Contracts/components/ContractCard/ContractCard.tsx
+++ client/src/pages/Contracts/components/ContractCard/ContractCard.tsx
@@ -15,7 +15,7 @@ const ContractCard: React.FC<OwnProps> = ({ contract }) => {
         <img src={paperClipIcon}></img>
       </div>
       <div className='image-wrapper'>
-        <img src={`${BASE_URL}/${contract.propertyDetail.images}`} />
+        <img src={`${BASE_URL}/${contract.propertyDetail?.images}`} />
       </div>
       <p className='title'>{contract.title}</p>
       <p className='created-at'>Created at: {contract.dateCreate}</p>
