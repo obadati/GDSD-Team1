65c893971f53d0a7d047d56fb3e6a58c195e2ac3
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-07-27 00:42:26 +0200

    Added code review to Carousel

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -4,34 +4,33 @@ import { BASE_URL } from "../../constants/constants";
 import "./Carousel.scss";
 
 interface CarouselProps {
-    images?: any[];
-    rounded?: boolean;
+  // Review: images should have proper types
+  images?: any[];
+  rounded?: boolean;
 }
 const CarouselComponent: React.FC<CarouselProps> = ({
-    images = [],
-    rounded = false,
+  images = [],
+  rounded = false,
 }) => {
-    return (
-        <div
-            className={`carousel-component carousel-component--${
-                rounded ? "rounded" : ""
-            }`}
-        >
-            <Carousel
-                animation="slide"
-                interval={3000}
-                cycleNavigation
-                autoPlay
-                indicators={false}
-            >
-                {images.map((item, i) => (
-                    <div className="slide" key={i}>
-                        <img src={`${BASE_URL}/${item}`}></img>
-                    </div>
-                ))}
-            </Carousel>
-        </div>
-    );
+  return (
+    <div
+      className={`carousel-component carousel-component--${
+        rounded ? "rounded" : ""
+      }`}>
+      <Carousel
+        animation='slide'
+        interval={3000}
+        cycleNavigation
+        autoPlay
+        indicators={false}>
+        {images.map((item, i) => (
+          <div className='slide' key={i}>
+            <img src={`${BASE_URL}/${item}`}></img>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default CarouselComponent;
