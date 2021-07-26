fae19ac76ccf670ab126452d8bb77cdd63b66821
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-07-27 00:42:35 +0200

    Added code review to ChatOnline

--- client/src/components/chatOnline/ChatOnline.tsx
+++ client/src/components/chatOnline/ChatOnline.tsx
@@ -1,24 +1,27 @@
+// Review: remove unused imports
+
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./ChatOnline.scss";
 
+/**
+ * Review: chatOnlineName is hardcoded
+ * Review: use arrow functions
+ * folder name should be capitalized
+ */
 export default function ChatOnline() {
-
-    return (
-        <div className="chatOnline">
-
-            <div className="chatOnlineFriend" >
-                <div className="chatOnlineImgContainer">
-                    <img
-                        className="chatOnlineImg"
-                        src="https://avatars.githubusercontent.com/u/80964042?v=4"
-                        alt=""
-                    />
-                    <div className="chatOnlineBadge"></div>
-                </div>
-                <span className="chatOnlineName">Ahmad Smith</span>
-            </div>
-
+  return (
+    <div className='chatOnline'>
+      <div className='chatOnlineFriend'>
+        <div className='chatOnlineImgContainer'>
+          <img
+            className='chatOnlineImg'
+            src='https://avatars.githubusercontent.com/u/80964042?v=4'
+          />
+          <div className='chatOnlineBadge'></div>
         </div>
-    );
+        <span className='chatOnlineName'>Ahmad Smith</span>
+      </div>
+    </div>
+  );
 }
