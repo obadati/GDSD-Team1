c476718f2a6698012789918535d5e9d5c3e48f77
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-09-28 18:43:29 +0200

    Show contracts as pending and approved

--- client/src/pages/Contracts/Contracts.tsx
+++ client/src/pages/Contracts/Contracts.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import {
   Contract,
+  ContractStatus,
   getAgentContracts,
   getBuyerContracts,
 } from "../../api/contracts";
@@ -15,12 +16,28 @@ import "./Contracts.scss";
 const ContractsPage: React.FC<PropsFromRedux> = ({ properties }) => {
   const { id, role } = useAuth();
   const [contracts, setContracts] = useState<Contract[]>([]);
+  const [pendingContracts, setPendingContracts] = useState<Contract[]>([]);
+  const [approvedContracts, setApprovedContracts] = useState<Contract[]>([]);
 
   useEffect(() => {
     if (id) {
       loadData();
     }
   }, [id]);
+
+  useEffect(() => {
+    if (contracts.length) {
+      const approved = contracts.filter(
+        (contract) => contract.status === ContractStatus.Approved
+      );
+      const pending = contracts.filter(
+        (contract) => contract.status === ContractStatus.Pending
+      );
+
+      setApprovedContracts(approved.length ? approved : []);
+      setPendingContracts(pending.length ? pending : []);
+    }
+  }, [contracts]);
 
   const loadData = async () => {
     const { result } = await (role === UserRoles.Agent
@@ -33,15 +50,35 @@ const ContractsPage: React.FC<PropsFromRedux> = ({ properties }) => {
     <div className='app-page contracts-page'>
       <h3 className='page-title'>Contracts</h3>
       <div className='contracts-wrapper'>
-        {contracts.length ? (
-          contracts.map((contract) => (
-            <ContractCard
-              contract={contract}
-              onContractUpdated={() => loadData()}
-            />
-          ))
+        {approvedContracts.length ? (
+          <>
+            <h5>Approved</h5>
+            <div className='contracts-group'>
+              {approvedContracts.map((contract) => (
+                <ContractCard
+                  contract={contract}
+                  onContractUpdated={() => loadData()}
+                />
+              ))}
+            </div>
+          </>
         ) : (
-          <p>No contracts yet!</p>
+          <p>No approved contracts yet!</p>
+        )}
+        {pendingContracts.length ? (
+          <>
+            <h5>Pending</h5>
+            <div className='contracts-group'>
+              {pendingContracts.map((contract) => (
+                <ContractCard
+                  contract={contract}
+                  onContractUpdated={() => loadData()}
+                />
+              ))}
+            </div>
+          </>
+        ) : (
+          <p>No pending contracts yet!</p>
         )}
       </div>
     </div>
