a528d98f622e3cb495e4e55b5aba10b3ddffd612
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-09-29 10:29:11 +0200

    Fix endpoint get messages

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,18 +1,18 @@
-import { AppRoutes } from '../containers/Router/routes';
+import { AppRoutes } from "../containers/Router/routes";
 export const TAG_LINE = "Welcome onboard from Team one!";
 export const DEBOUNCE_DURATION = 300;
 export const AUTH_USER_KEY = "auth-user";
 export const BASE_URL = "https://www.homeforme-aws.de:5000";
 export const RESULTS_PER_PAGE = 8;
 export const NavigationTabData = [
-    { label: 'home', to: AppRoutes.Landing },
-    { label: 'properties', to: AppRoutes.Properties },
-    { label: 'average price', to: AppRoutes.AvgPrice },
-    { label: 'Dashboard', to: AppRoutes.Dashboard },
-    { label: 'companies', to: AppRoutes.Companies },
-    { label: 'about us', to: AppRoutes.AboutUs },
-  ];
-export const API_MSG_URL = '/api/message/getNewMessages';
+  { label: "home", to: AppRoutes.Landing },
+  { label: "properties", to: AppRoutes.Properties },
+  { label: "average price", to: AppRoutes.AvgPrice },
+  { label: "Dashboard", to: AppRoutes.Dashboard },
+  { label: "companies", to: AppRoutes.Companies },
+  { label: "about us", to: AppRoutes.AboutUs },
+];
+export const API_MSG_URL = "/api/message/getNewMessages/";
 export const LOG_OUT = "Log Out";
 export const MAX_ROOMS_ALLOWED = 8;
 export const MIN_ROOMS_ALLOWED = 1;
